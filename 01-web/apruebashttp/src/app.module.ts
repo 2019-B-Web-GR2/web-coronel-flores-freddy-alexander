6299fad1b5b76fe8fa738552b9c4ed15efcc9848
Alexander <freddy.coronel@epn.edu.ec>
2019-12-13 16:00:52 -0500

    getting started with rest in the user controller

--- 01-web/apruebashttp/src/app.module.ts
+++ 01-web/apruebashttp/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {UsuarioEntity} from "./usuario/usuario.entity";
 import {UsuarioModule} from "./usuario/usuario.module";
+import {UsuarioService} from "./usuario/usuario.service";
 
 @Module({
   imports: [
@@ -15,15 +16,35 @@ import {UsuarioModule} from "./usuario/usuario.module";
       port: 32769,
       username: 'root',
       password: 'alex1995',
+      dropSchema: true,
       database: 'web',
       entities: [
           UsuarioEntity,
       ],
-      synchronize: true
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
   providers: [AppService],
   
 })
-export class AppModule {}
+export class AppModule {
+    constructor( private _usuarioService: UsuarioService) {
+
+        /* const usuarioPromesa = this._usuarioService.encontrarUno(1);
+        console.log('empieza')
+        console.log(usuarioPromesa)
+        usuarioPromesa
+            .then(
+                (data) => {
+                    console.log('data',data)
+                }
+            )
+            .catch((error) => {
+                console.log('error',error)
+            });
+
+        console.log('termina en orden')
+        */
+    }
+}
--- 01-web/apruebashttp/src/usuario/usuario.service.ts
+++ 01-web/apruebashttp/src/usuario/usuario.service.ts
@@ -1,7 +1,62 @@
 import {Injectable} from "@nestjs/common";
+import {InjectRepository} from "@nestjs/typeorm";
+import {UsuarioEntity} from "./usuario.entity";
+import {DeleteResult, Repository} from "typeorm";
 
 @Injectable()
 export  class UsuarioService {
+    constructor(
+        // inyectar las dependencias
+        // cualquier clase se puede inyectar
+        @InjectRepository(UsuarioEntity) private _repositorioUsuario: Repository<UsuarioEntity>,
+    ){}
+
+    encontrarUno(id: number): Promise<UsuarioEntity> | undefined{
+        const usuario = this._repositorioUsuario.findOne(id);
+        // transformar la programación asincrona en sincrona con async y await
+        return usuario;
+    }
+
+    guardarUno(usuario): Promise<UsuarioEntity | undefined> {
+        // @ts-ignore
+        const usuarioGuardado = this._repositorioUsuario.save<UsuarioEntity[]>(usuario);
+        return usuarioGuardado;
+    }
+
+    borrarUno(id: number): Promise<DeleteResult>{
+        return this._repositorioUsuario.delete(id);
+    }
+
+    actualizarUno(id: number, usuario) {
+        usuario.id = id;
+        return this._repositorioUsuario.save(usuario); // upsert
+    }
+
+    buscar(
+        whereNormal = {},
+        skip: number= 0,
+        take: number= 10,
+    ){
+        this._repositorioUsuario.find(
+            {
+                // where: {
+                 //    cedula: '48248',
+                //},
+                where: [
+                    {
+                        nombre: 'alex'                    },
+                    {
+                        cedula: '4548',
+                    }
+                ],
+                skip: 0,
+                take: 10,
+            },
+        )
+    }
+
+
+
 
 
 }
